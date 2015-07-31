572d98802611b6565970097dd3f7e89c26169176
Christopher Davis <omegazero2020@gmail.com>
2015-07-31 18:09:56 -0500

    Halfway through unit testing

--- UI2HTMLTests/UI2HTMLTest.m
+++ UI2HTMLTests/UI2HTMLTest.m
@@ -11,6 +11,7 @@
 #import <Quick/Quick.h>
 #import <Nimble/Nimble.h>
 @import Nimble;
+
 #import "UI2HTML_Tag.h"
 
 static NSString *divTag = @"div";
@@ -32,59 +33,127 @@ __block UI2HTML_Tag * tag;
     qck_describe(@"-init", ^{
         it(@"should contain empty strings", ^{
             tag = [[UI2HTML_Tag alloc] init];
-            expect([tag tagString]).to(beNil());
-            expect([tag idString]).to(beNil());
-            expect([tag classString]).to(beNil());
-            expect([tag customStyle]).to(beNil());
+            expect([tag tagString]).to(beEmpty());
+            expect([tag idString]).to(beEmpty());
+            expect([tag classString]).to(beEmpty());
+            expect([tag customStyle]).to(beEmpty());
         });
     });
     qck_describe(@"-initWithTag", ^{
-        it(@"should only set tagString with String", ^{
+        it(@"should only set tagString", ^{
             tag = [[UI2HTML_Tag alloc] initWithTag:divTag];
             expect([tag tagString]).to(equal(divTag));
-            expect([tag idString]).to(beNil());
-            expect([tag classString]).to(beNil());
-            expect([tag customStyle]).to(beNil());
+            expect([tag idString]).to(beEmpty());
+            expect([tag classString]).to(beEmpty());
+            expect([tag customStyle]).to(beEmpty());
         });
     });
     qck_describe(@"-initWithTagObject", ^{
-            it(@"should only set tagString with Class", ^{
+            it(@"should only set tagString", ^{
             //expecting lower case class string without "_UI2HTML"
             tag = [[UI2HTML_Tag alloc] initWithTagObject:[Div_UI2HTML class]];
             expect([tag tagString]).to(equal(divTag));
-            expect([tag idString]).to(beNil());
-            expect([tag classString]).to(beNil());
-            expect([tag customStyle]).to(beNil());
+            expect([tag idString]).to(beEmpty());
+            expect([tag classString]).to(beEmpty());
+            expect([tag customStyle]).to(beEmpty());
         });
     });
-    qck_describe(@"-initWithTag:Class:", ^{
-        it(@"should only set tagString with Class", ^{
-            //expecting lower case class string without "_UI2HTML"
-            tag = [[UI2HTML_Tag alloc] initWithTagObject:[Div_UI2HTML class]];
+    qck_describe(@"-initWithTag:class:", ^{
+        it(@"should only set tagString and classString", ^{
+            tag = [[UI2HTML_Tag alloc] initWithTag:divTag class:classTag1];
             expect([tag tagString]).to(equal(divTag));
-            expect([tag idString]).to(beNil());
-            expect([tag classString]).to(beNil());
-            expect([tag customStyle]).to(beNil());
+            expect([tag idString]).to(beEmpty());
+            expect([tag classString]).to(equal(classTag1));
+            expect([tag customStyle]).to(beEmpty());
         });
     });
-qck_describe(@"-initWithTagObject", ^{
-    it(@"should only set tagString with Class", ^{
-        //expecting lower case class string without "_UI2HTML"
-        tag = [[UI2HTML_Tag alloc] initWithTagObject:[Div_UI2HTML class]];
-        expect([tag tagString]).to(equal(divTag));
-        expect([tag idString]).to(beNil());
-        expect([tag classString]).to(beNil());
-        expect([tag customStyle]).to(beNil());
+    qck_describe(@"-initWithTagObject:class:", ^{
+        it(@"should only set tagString and classString", ^{
+            tag = [[UI2HTML_Tag alloc] initWithTagObject:[Div_UI2HTML class] class:classTag1];
+            expect([tag tagString]).to(equal(divTag));
+            expect([tag idString]).to(beEmpty());
+            expect([tag classString]).to(equal(classTag1));
+            expect([tag customStyle]).to(beEmpty());
+        });
     });
-});qck_describe(@"-initWithTagObject", ^{
-    it(@"should only set tagString with Class", ^{
-        //expecting lower case class string without "_UI2HTML"
-        tag = [[UI2HTML_Tag alloc] initWithTagObject:[Div_UI2HTML class]];
-        expect([tag tagString]).to(equal(divTag));
-        expect([tag idString]).to(beNil());
-        expect([tag classString]).to(beNil());
-        expect([tag customStyle]).to(beNil());
+    qck_describe(@"-initWithTag:class:id:", ^{
+        it(@"should only set tagString, classString, and idString", ^{
+            tag = [[UI2HTML_Tag alloc] initWithTag:divTag class:classTag1 id:idTag];
+            expect([tag tagString]).to(equal(divTag));
+            expect([tag idString]).to(equal(idTag));
+            expect([tag classString]).to(equal(classTag1));
+            expect([tag customStyle]).to(beEmpty());
+        });
     });
-});
+    qck_describe(@"-initWithTagObject:class:id:", ^{
+        it(@"should only set tagString, classString, and idString", ^{
+            tag = [[UI2HTML_Tag alloc] initWithTagObject:[Div_UI2HTML class] class:classTag1 id:idTag];
+            expect([tag tagString]).to(equal(divTag));
+            expect([tag idString]).to(equal(idTag));
+            expect([tag classString]).to(equal(classTag1));
+            expect([tag customStyle]).to(beEmpty());
+        });
+    });
+
+    qck_describe(@"-setClasses:", ^{
+        __block UI2HTML_Tag *tag = nil;
+        beforeEach(^{
+            tag = [[UI2HTML_Tag alloc] init];
+        });
+        it(@"should be nil from nil Array", ^{
+            [tag setClasses:nil];
+            expect([tag classString]).to(beEmpty());
+        });
+        it(@"should be nil from empty Array", ^{
+            [tag setClasses:@[]];
+            expect([tag classString]).to(beEmpty());
+        });
+        it(@"should equal one class", ^{
+            [tag setClasses:@[classTag1]];
+            expect([tag classString]).to(equal(classTag1));
+        });
+        it(@"should contain two classes", ^{
+            [tag setClasses:@[classTag1, classTag2]];
+            expect([tag classString]).to(contain(classTag1));
+            expect([tag classString]).to(contain(classTag2));
+        });
+        it(@"should not contain duplicate classes", ^{
+            [tag setClasses:@[classTag1, classTag2]];
+            [tag setClasses:@[classTag1, classTag2, classTag3]];
+            expect([tag classString]).to(contain(classTag1));
+            expect([tag classString]).to(contain(classTag2));
+        });
+    });
+
+    //TODO: look up behave like
+    qck_describe(@"-addClasses:", ^{
+        __block UI2HTML_Tag *tag = nil;
+        beforeEach(^{
+            tag = [[UI2HTML_Tag alloc] init];
+        });
+        it(@"should be nil from nil Array", ^{
+            [tag addClasses:nil];
+            expect([tag classString]).to(beEmpty());
+        });
+        it(@"should be nil from empty Array", ^{
+            [tag addClasses:@[]];
+            expect([tag classString]).to(beEmpty());
+        });
+        it(@"should equal one class", ^{
+            [tag addClasses:@[classTag1]];
+            expect([tag classString]).to(equal(classTag1));
+        });
+        it(@"should contain two classes", ^{
+            [tag addClasses:@[classTag1, classTag2]];
+            expect([tag classString]).to(contain(classTag1));
+            expect([tag classString]).to(contain(classTag2));
+        });
+        it(@"should not contain duplicate classes", ^{
+            [tag addClasses:@[classTag1, classTag2]];
+            [tag addClasses:@[classTag1, classTag2, classTag3]];
+            expect([tag classString]).to(contain(classTag1));
+            expect([tag classString]).to(contain(classTag2));
+        });
+    });
 
 QuickSpecEnd
