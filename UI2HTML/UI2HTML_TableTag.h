b2efa5845adf23e6896234f08f916830215a16b6
Christopher Davis <omegazero2020@gmail.com>
2015-08-09 15:10:58 -0500

    Completed first two form scenes

---
+++ UI2HTML/UI2HTML_TableTag.h
@@ -0,0 +1,13 @@
+//
+//  UI2HTML_TableTag.h
+//  UI2HTML
+//
+//  Created by Christopher Davis on 8/4/15.
+//  Copyright (c) 2015 Christopher Davis. All rights reserved.
+//
+
+#import "UI2HTML_Tag.h"
+
+@interface UI2HTML_TableTag : UI2HTML_Tag
+
+@end
